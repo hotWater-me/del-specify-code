8a35a865a979e23895af50743644f6720fe92caa
wangYP <wb-wyp737365@antfin.com>
2021-08-09 17:50:00 +0800

    fea: 支持单文件

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const list = [];
 const curProcess = process.argv.slice(2);
-if(curProcess[0] === '-v') {
-  
+if (curProcess[0] === '-v') {
   process.exit(0);
 }
 const opt = curProcess.filter((ele) => ele.substring(0, 5).indexOf('-') !== -1);
@@ -28,10 +27,11 @@ let setFile = allParam.find(
   (item) => item.opt === '--path' || item.opt === '-p',
 );
 let setReg = allParam.find((item) => item.opt === '--reg' || item.opt === '-r');
+
 function listFile(dir) {
-  if(dir.charAt(0) === '.') {
-   dir = path.join(process.cwd(), dir);
-  };
+  if (dir.charAt(0) === '.') {
+    dir = path.join(process.cwd(), dir);
+  }
   const arr = fs.readdirSync(dir);
   arr.forEach(function (item) {
     var fullpath = path.join(dir, item);
@@ -44,7 +44,7 @@ function listFile(dir) {
   });
   return list;
 }
-const delConsole = (filePath, regParam) => {
+const delMatch = (filePath, regParam) => {
   filePath.forEach((ele) => {
     const data = fs.readFileSync(ele);
     const parse = data.toString().replace(eval(regParam), '');
@@ -52,16 +52,22 @@ const delConsole = (filePath, regParam) => {
   });
 };
 
+const delFile = (singlefile, regParam) => {
+  const data = fs.readFileSync(singlefile);
+  const parse = data.toString().replace(eval(regParam), '');
+  fs.writeFileSync(singlefile, parse);
+}
+
 if (setFile && setReg && setFile.param && setReg.param) {
   new Promise((resolve) => {
     resolve();
   }).then(() => {
-    delConsole(listFile(setFile.param), setReg.param);
-    
+    if (fs.lstatSync(setFile.param).isFile()) {
+      delFile(setFile.param, setReg.param)
+    } else {
+      delMatch(listFile(setFile.param), setReg.param);
+    }
   });
 } else {
-  
   process.exit(1);
-}
-
-// /console\.log\(.*\)/g
+};
